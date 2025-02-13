ae9ecb8cf6d9ef58578cff0a7f334f7bae740dfb
Thierry Carrard <thierry.carrard@orange.fr>
2025-02-13 18:27:38 +0100

    output bad bib id with special mark

--- python/racing_bib_finish.py
+++ python/racing_bib_finish.py
@@ -3,11 +3,14 @@
 import time
 import sys
 
+# Categories for each birth year
+CAT_YEAR = { 2019: 'BB' , 2020: 'BB' , 2016: 'EA' , 2017: 'EA' , 2018: 'EA' , 2014: 'PO' , 2015: 'PO' , 2012: 'BE' , 2013: 'BE' , 2010: 'MI' , 2011: 'MI' }
+
 # first, get that race start time
 START_TIME = time.perf_counter()
 RUNNERS_FILE = sys.argv[1]
 RESULT_FILE = sys.argv[2]
-print("read runners from %s, write results to %s"%(RUNNERS_FILE,RESULT_FILE))
+print("Runners <- %s\nResults -> %s"%(RUNNERS_FILE,RESULT_FILE))
 
 # then, we have a little time to initialize everything
 BIB_CODE_LEN=13
@@ -17,7 +20,9 @@ CATRANK={}
 for r in RUNNERS_LIST:
     RUNNERS[ int(r[0]) ] = r
     CATRANK[r[5]] = 1
-print(RUNNERS)
+
+for (k,v) in RUNNERS.items():
+    print(k,v)
 
 ofile = open(RESULT_FILE,'a+')
 
@@ -29,17 +34,23 @@ START_TIME = time.perf_counter()
 rank=1
 BIB_CODE = input()
 while BIB_CODE != "end":
-    ELAPSED = time.perf_counter() - START_TIME
+    SECONDS = time.perf_counter() - START_TIME
+    MINUTES = int(SECONDS)//60
+    SECONDS -= MINUTES * 60
+    HOURS = MINUTES//60
+    MINUTES -= HOURS*60
     BIB_ID=int(BIB_CODE)
+    output_line = "";
     if not BIB_ID in RUNNERS.keys():
-        print("---- : #%04d not in base"%BIB_ID)
+        output_line = "%04d ; %02d:%02d:%05.2f ; ?%d ; ; ; " % (rank,HOURS,MINUTES,SECONDS,BIB_ID)
     else:
         runner = RUNNERS[BIB_ID]
         crank = CATRANK[runner[5]]
-        output_line = "%04d ; %05.2f ; #%04d ; %.20s %.20s ; %s ; %d" % (rank,ELAPSED,BIB_ID,runner[3],runner[4],runner[5],crank)
-        print(output_line)
-        ofile.write(output_line+'\n')
-        rank = rank + 1
-        CATRANK[runner[5]] = crank + 1
+        output_line = "%04d ; %02d:%02d:%05.2f ; #%.13s ; %.20s %.20s ; %s ; %d" % (rank,HOURS,MINUTES,SECONDS,runner[1],runner[3],runner[4],runner[5],crank)
+    print(output_line)
+    ofile.write(output_line+'\n')
+    ofile.flush()
+    rank = rank + 1
+    CATRANK[runner[5]] = crank + 1
     BIB_CODE = input()
 

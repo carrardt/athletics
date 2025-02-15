980943f3f65b00d3217cfd609c732c82ecff9c98
Thierry Carrard <thierry.carrard@cea.fr>
2025-02-15 08:49:10 +0100

    updated race start script

--- python/start_race.sh
+++ python/start_race.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-RACENAME="Cource1K"
+RACENAME="$1"
 
-xterm -T "${RACENAME}" -geometry 20x2+0+0 -fa 'Monospace' -fs 32 -e "./wall_clock.py" &
+xterm -T "${RACENAME} CHRONO" -geometry 20x2+0+0 -fa 'Monospace' -fs 32 -e "./wall_clock.py" &
+xterm -T "${RACENAME} FINISH" -geometry 100x30+0+180 -fa 'Monospace' -fs 18 -e "./racing_bib_finish.py ${RACENAME}"
 
 wait
 echo "Finished"

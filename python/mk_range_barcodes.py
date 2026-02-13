d4b79f88a1edc012d840e70a2222b5f04f572c6b
Thierry Carrard <thierry.carrard@cea.fr>
2026-02-13 15:42:09 +0100

    generator for full bib with text + barcode

--- python/mk_range_barcodes.py
+++ python/mk_range_barcodes.py
@@ -2,10 +2,14 @@
 
 import os
 import barcode
+import sys
+
+BS=int(sys.argv[1])
+BE=int(sys.argv[2])+1
 
 OUTPUT_SVG="bib_%04d.svg"
 
-for i in range(260,360):
+for i in range(BS,BE):
     barcode_data = "%012d" % i
     print(barcode_data)
     barcode.EAN13(barcode_data,writer=barcode.writer.SVGWriter()).write(open(OUTPUT_SVG%i,'wb'))
